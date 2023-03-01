f20578b7aaa2e845e0319b2e9846e8cb90e79617
emreharman <emrehrmnb@gmail.com>
2023-03-01 23:33:24 +0300

    redux structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 function App() {
+  /* const booksState=useSelector(state=>state.booksState)
+  const categoriesState=useSelector(state => state.categoriesState) */
+  const {booksState}=useSelector(state=>state)
+
+ 
+  console.log(booksState);
   return (
     <div>
       <h1>Library App start</h1>
@@ -9,3 +17,15 @@ function App() {
 }
 
 export default App;
+/* const obje={
+  obje1:{
+    name:"emre"
+  },
+  obje2:{
+    name:"ahmet"
+  }
+}
+const obje1=obje.obje1
+const obje2=obje.obje2
+
+const {obje1,obje2}=obje */
